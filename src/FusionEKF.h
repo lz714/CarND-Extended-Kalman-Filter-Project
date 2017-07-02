b322c40b7676930f236cb38dc11d041e27858d38
IC\lz714 <zhulin199228@gmail.com>
2017-07-02 19:59:15 +0100

    submission

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -40,10 +40,13 @@ private:
 
   // tool object used to compute Jacobian and RMSE
   Tools tools;
-  Eigen::MatrixXd R_laser_;
-  Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
+  Eigen::MatrixXd R_laser_;    // laser measurement noise
+  Eigen::MatrixXd R_radar_;    // radar measurement noise
+  Eigen::MatrixXd H_laser_;    // measurement function for laser
+  Eigen::MatrixXd H_jacobian;         // measurement function for radar
+
+  float noise_ax;
+  float noise_ay;
 };
 
 #endif /* FusionEKF_H_ */
